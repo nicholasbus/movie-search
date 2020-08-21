490eca11cc2433443a0c55649952327fc5abd73c
Nicholas Smith <nicholassmithbus@gmail.com>
2020-08-20 23:23:35 -0600

    api key in api url

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
   const handleSubmit = () => {
     axios({
       method: 'get',
-      url: `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1&include_adult=false&query=${queryInput}`
+      url: `https://api.themoviedb.org/3/search/movie?api_key=7fe59f88be383a3675b8713834c00c73&language=en-US&page=1&include_adult=false&query=${queryInput}`
     })
     .then(res => {
       setMovieInfo(res.data.results)
